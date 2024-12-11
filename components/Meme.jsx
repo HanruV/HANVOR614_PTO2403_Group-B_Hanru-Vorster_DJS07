e8d193c5779e2bbbb39196813301555e55a40844
Hanru Vorster <hanru.vorster@gmail.com>
2024-12-11 11:12:25 +0200

    added state form memeImage generation, and added a img element to the meme.jsx component

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
       <div className="form">
@@ -23,8 +31,11 @@ export default function Meme() {
             className="form--input"
           />
         </div>
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
       </div>
+      <img className="meme--image" src={memeImage}></img>
     </main>
   );
 }
