3cc70a6af17e9e1a9cb38f8d7d54c9829d8214ed
Hanru Vorster <hanru.vorster@gmail.com>
2024-12-11 10:10:07 +0200

    changed the form to a div element

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -4,7 +4,7 @@ import memesData from "../memesData";
 export default function Meme() {
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <div>
           <label htmlFor="top-text">Top Text</label>
           <input
@@ -24,7 +24,7 @@ export default function Meme() {
           />
         </div>
         <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 }
