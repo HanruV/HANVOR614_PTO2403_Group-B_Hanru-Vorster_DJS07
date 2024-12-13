e1c211bb953b7dea7c2a1e9165864ecd8857b315
Hanru Vorster <hanru.vorster@gmail.com>
2024-12-13 08:58:02 +0200

    added headings to the meme images generated and posted state to them with a handleChange()

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -20,6 +20,14 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
@@ -30,6 +38,9 @@ export default function Meme() {
             type="text"
             placeholder="Shut up"
             className="form--input"
+            name="topText"
+            value={meme.topTextText}
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -39,13 +50,20 @@ export default function Meme() {
             type="text"
             placeholder="and take my money"
             className="form--input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
           />
         </div>
         <button onClick={getMemeImage} className="form--button">
           Get a new meme image 🖼
         </button>
       </div>
-      <img className="meme--image" src={meme.randomImage}></img>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
